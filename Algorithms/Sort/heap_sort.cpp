22d55ba06f3e85bba01a11f09ce8e3ae2e2a9c3e
marktubu <marktubu0@gmail.com>
2019-08-19 03:41:58 +0800

    prio queue

--- Algorithms/Sort/heap_sort.cpp
+++ Algorithms/Sort/heap_sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
@@ -11,8 +12,39 @@ using namespace std;
 
 #define LEFT(i) ((i << 1) + 1)
 #define RIGHT(i) ((i << 1) + 2)
-#define PARENT(i) ((i-1) >> 1)
+#define PARENT(i) (i == 0 ? -1 : (i-1) >> 1)
 
+
+
+//建立最大堆的操作
+template<unsigned N>
+void max_heapify_max_heap(int (&arr) [N], int i, int heap_size)
+{
+    int l = LEFT(i);
+    int r = RIGHT(i);
+
+    int min = i;
+    if(l < heap_size && arr[l] < arr[min])
+    {
+        min = l;
+    }
+
+    if(r < heap_size && arr[r] < arr[min])
+    {
+        min = r;
+    }
+
+    if(min != i)
+    {
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+
+        max_heapify_max_heap(arr, min, heap_size);
+    }
+}
+
+//建立最小堆的操作
 template<unsigned N>
 void max_heapify(int (&arr) [N], int i, int heap_size)
 {
@@ -46,7 +78,7 @@ void build_max_heap(int (&arr) [N], int heap_size)
     int size = N/2-1;
     for(int i=size;i>=0;i--)
     {
-        max_heapify(arr, i, heap_size);
+        max_heapify_max_heap(arr, i, heap_size);
     }
 }
 
@@ -59,7 +91,7 @@ void heap_sort(int (&arr)[N], int heap_size)
     }
 
     build_max_heap(arr, heap_size);
-
+    cout << "max " << arr[0] << endl;
     int temp = arr[0];
     arr[0] = arr[heap_size-1];
     arr[heap_size-1] = temp;
@@ -69,13 +101,67 @@ void heap_sort(int (&arr)[N], int heap_size)
     heap_sort(arr, heap_size);
 }
 
+template<unsigned N>
+void increase_key(int (&arr)[N], int x, int k)
+{
+    if(arr[x] < k)
+    {
+        arr[x] = k;
+        while(PARENT(x) >=0 && arr[PARENT(x)] < k)
+        {
+            int value = arr[x];
+            arr[x] = arr[PARENT(x)];
+            arr[PARENT(x)] = value;
+
+            x = PARENT(x);
+        }
+    }
+}
+
+template<unsigned N>
+void insert(int (&arr)[N], int key, int heap_size)
+{
+    heap_size ++;
+    arr[heap_size] = INT_MIN;
+
+    increase_key(arr, heap_size, key);    
+}
+
+template<unsigned N>
+int maximum(int (&arr)[N])
+{
+    return arr[0];
+}
+
+template<unsigned N>
+int extract_max(int (&arr)[N], int heap_size)
+{
+    int max = arr[0];
+    arr[0] = arr[heap_size-1];
+    arr[heap_size-1] = 0;
+    heap_size--;
+    max_heapify(arr, 0, heap_size);
+    return max;
+}
+
 int main()
 {
-    int array[] = {31, 41, 59,26, 41, 58, 2, 4, 7, 1, 23, 123};
-    heap_sort(array, 12);
-
-    for(int i=0;i<12;++i)
+    int array[] = {31, 41, 59,26, 41, 58, 2, 4, 0, 0};
+    heap_sort(array, 8);
+    for(int i=0;i<10;++i)
+    {
+        cout << "array sort : " << array[i] << endl;
+    }
+    int max = extract_max(array, 8);
+    cout << "max 222" << max <<endl;
+    for(int i=0;i<10;++i)
     {
         cout << "array : " << array[i] << endl;
+    }
+
+    insert(array, 100, 7);
+    for(int i=0;i<10;++i)
+    {
+        cout << "array after insert : " << array[i] << endl;
     }
 }
