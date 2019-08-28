f04936d2049f2a12e104893a9a7117a83d3928ad
marktubu <mark@tubu.com>
2019-08-28 10:03:46 +0000

    part of part3

--- DB/main.c
+++ DB/main.c
@@ -21,11 +21,6 @@ typedef enum
     STATEMENT_INSERT, STATEMENT_SELECT
 } StatementType;
 
-typedef struct 
-{
-    StatementType type;
-} Statement;
-
 typedef void (*PFunc)(void* arg);
 
 typedef struct 
@@ -41,6 +36,71 @@ typedef struct
     PFunc cmd_func;
     int cmd_num;
 } Command;
+
+#define COLUME_USERNAME_SIZE 32
+#define COLUME_EMAIL_SE
+typedef struct 
+{
+    uint32_t id;
+    char username[COLUME_USERNAME_SIZE];
+    char email[COLUME_EMAIL_SIZE];
+} Row;
+
+
+typedef struct 
+{
+    StatementType type;
+    Row row_to_insert;
+} Statement;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+void serialize_row(Row* source, void* destination)
+{
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination)
+{
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct 
+{
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+void * row_slot(Table* table, uint32_t row_num)
+{
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if(page == NULL)
+    {
+        page = malloc(PAGE_SIZE);
+        table->pages[page_num] = page;
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
 
 InputBuffer* new_input_buffer()
 {
