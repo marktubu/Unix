82036752cc924edfeb8d5c9456c2f910473b46df
marktubu <marktubu0@gmail.com>
2019-10-07 21:26:25 +0800

    dp15_1 top down

--- Algorithms/Theory/dp15_1.cpp
+++ Algorithms/Theory/dp15_1.cpp
@@ -4,24 +4,38 @@
 
 using namespace std;
 
-int cut(const vector<int>& p, int n)
+int cut_aux(const vector<int>& p, int n, vector<int>& r)
 {
+    if(r[n] >= 0)
+        return r[n];
+    int q = -1;
     if(n == 0)
-        return 0;
-    
-    int q = 0;
-    for(int i=1;i<=n;i++)
     {
-        q = max(q,p[i] + cut(p, n - i));
+        q = 0;
     }
-
+    else
+    {
+        for (size_t i = 1; i <= n; i++)
+        {
+            q = max(q, p[i] + cut_aux(p, n-i, r));
+        }
+    }
+    
+    r[n] = q;
     return q;
+}
+
+int cut(const vector<int>& p, int n, vector<int>& r)
+{
+    return cut_aux(p, n, r);
 }
 
 int main()
 {
-    vector<int> vec{1,5,8,9,10,17,18, 20,24,30,35,40,45,50,55,60};
-    int ret = cut(vec, 12);
+    vector<int> vec{0,1,5,8,9,10,17,18, 28,29,30,31,32,45,50,55,60};
+    int n = 12;
+    vector<int> r(n+1, -1);
+    int ret = cut(vec, n, r);
     cout << "result : " << ret << endl;
     return 0;
 }
