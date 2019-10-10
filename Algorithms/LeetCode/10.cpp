173a7a56e621684efb50272f3d82aea62c183adc
marktubu <marktubu0@gmailcom>
2019-10-10 18:17:10 +0800

    10

--- Algorithms/LeetCode/10.cpp
+++ Algorithms/LeetCode/10.cpp
@@ -9,16 +9,33 @@
 
 using namespace std;
 
-enum State
-{
-    Letter,
-    Dot,
-    Star
-}
-
 bool isMatch(string s, string p) 
 {
-    
+    if(s.empty())
+    {
+        return p.empty() || (p.length() == 2 && p[1] == '*');
+    }
+
+    if(p.empty())
+    {
+        return false;
+    }
+
+    bool first_match = s[0] == p[0] || p[0] == '.';
+
+    if(p.length() >= 2 && p[1] == '*')
+    {
+        string s_1 = s.substr(1, s.size());  
+        string p_2 = p.substr(2, p.size());  
+        cout << "s1 " << s_1 << endl;
+        return isMatch(s, p_2) || first_match && isMatch(s_1, p);
+    }
+    else
+    {
+        string s_1 = s.substr(1, s.size());  
+        string p_1 = p.substr(1, p.size()); 
+        return first_match && isMatch(s_1, p_1);
+    }
 }
 
 int main(int argc, char* argv[])
@@ -30,8 +47,8 @@ int main(int argc, char* argv[])
     char* arg2 = argv[2];
     cout << "arg " << arg1 << "  " << arg2 << endl;
     
-    string str1(arg1);
-    string str2(arg2);
+    string str1("aa");
+    string str2("a*");
 
     bool ret = isMatch(str1, str2);
     cout << "reverse : " << ret << endl;
