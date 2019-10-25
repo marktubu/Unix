288cd0e030514ff1371bb28cd18ab6757dca4645
marktubu <marktubu0@gmailcom>
2019-10-25 17:28:26 +0800

    shared_ptr

--- MySTL/MySTL/Memory.h
+++ MySTL/MySTL/Memory.h
@@ -147,4 +147,131 @@ namespace MySTL
 	unique_ptr<T> make_unique(Args&& ... args) {
 		return unique_ptr<T>(new T(std::forward<Args>(args)...));
 	}
+
+
+	template<class T>
+	class shared_ptr
+	{
+	public:
+		typedef T element_type;
+		typedef T* raw_pointer;
+		typedef size_t size_type;
+
+	private:
+		template<class Type>
+		using ref_t = Impl::ref_t<Type>;
+	public:
+		shared_ptr(raw_pointer p) :_ref(new ref_t<T>(p)) {}
+
+		template<class D>
+		shared_ptr(raw_pointer p, D del) :_ref(new ref_t<T>(p, del)) {}
+
+		shared_ptr(const shared_ptr& sp)
+		{
+			copy_ref(sp._ref);
+		}
+
+		~shared_ptr() { decrease_ref(); }
+
+		shared_ptr& operator=(const shared_ptr& sp)
+		{
+			if (this != &sp)
+			{
+				decrease_ref();
+				copy_ref(sp._ref);
+			}
+			
+			return *this;
+		}
+	public:
+		ref_t<T>* _ref;
+
+	public:
+		raw_pointer operator->() { return get(); }
+		element_type& operator*() { return *get(); }
+		const raw_pointer operator->()const { return get(); }
+		const element_type& operator*()const { return *get(); }
+
+		operator bool() { return get() != nullptr; }
+
+		void reset(raw_pointer p)
+		{
+			if (p != get())
+			{
+				decrease_ref();
+			}
+		}
+
+		raw_pointer get() { return _ref->get_data(); }
+		const raw_pointer get()const { return _ref->get_data(); }
+		size_type use_count() { return _ref->count(); }
+	private:
+		//拷贝引用
+		void copy_ref(ref_t<T>* ref)
+		{
+			_ref = ref;
+			++(*ref);
+		}
+
+		//减引用
+		void decrease_ref()
+		{
+			if (_ref->get_data())
+			{
+				--(*_ref);
+				if (use_count() == 0)
+				{
+					delete _ref;
+				}
+			}
+		}
+	};
+
+	template <class T, class... Args>
+	shared_ptr<T> make_shared(Args&& ... args)
+	{
+		return shared_ptr<T>(new T(std::forward<Args>(args)...));
+	}
+
+	template<class D, class T>
+	D* get_deleter(const shared_ptr<T>& sp)
+	{
+		return sp._ref->_deleter;
+	}
+
+	template<class T1, class T2>
+	bool operator == (const shared_ptr<T1>& lhs, const shared_ptr<T2>& rhs) 
+	{
+		return lhs.get() == rhs.get();
+	}
+
+	template<class T>
+	bool operator == (const shared_ptr<T>& sp, nullptr_t p) 
+	{
+		return sp.get() == p;
+	}
+
+	template<class T>
+	bool operator == (nullptr_t p, const shared_ptr<T>& sp) 
+	{
+		return sp == p;
+	}
+
+	template<class T1, class T2>
+	bool operator != (const shared_ptr<T1>& lhs, const shared_ptr<T2>& rhs) 
+	{
+		return !(lhs == rhs);
+	}
+
+	template<class T>
+	bool operator != (const shared_ptr<T>& sp, nullptr_t p) 
+	{
+		return !(sp == p);
+	}
+
+	template<class T>
+	bool operator != (nullptr_t p, const shared_ptr<T>& sp) 
+	{
+		return !(sp == p);
+	}
 }
