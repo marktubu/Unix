97806610fb7bcce1f4697fa5f3aa4c57f49e93b7
marktubu <marktubu0@gmailcom>
2019-10-30 10:46:58 +0800

    12

--- Algorithms/LeetCode/12.cpp
+++ Algorithms/LeetCode/12.cpp
@@ -1,45 +1,76 @@
+
 #include <iostream>
-#include <string.h>
-#include <cstdint>
 #include <vector>
-#include <limits.h>
-#include <sstream> 
 #include <string>
-#include <stdlib.h>
-#include <algorithm>    // std::min
+#include <algorithm> 
+#include <cmath>
 
 using namespace std;
 
 string intToRoman(int num) 
 {
-    vector<char> table = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-    string str_num = to_string(num);
-    int len = str_num.size();
+    if(num == 0)
+        return "";
+    vector<string> table = {"I", "V", "X", "L", "C", "D", "M"};
+    int len = 0;
+    if(num > 999)
+    {
+        len = 4;
+    }
+    else if(num > 99)
+    {
+        len = 3;
+    }
+    else if(num > 9)
+    {
+        len = 2;
+    }
+    else
+    {
+        len = 1;
+    }
+    
     int base = 2 * len - 2;
-    int max_num = std::pow(10, len-1);
+    int factor = len - 1;
+    //pow(10,2) = 99.999... 转换为int 为99，因此需要round
+    int min_num = std::round(pow(10, factor));
 
     string ret;
     
-    int left_num = (int) num / max_num;
-    if(left_num >= 5)
+    int left_num = (int) num / min_num;
+    cout << base << endl;
+    if(left_num == 9)
     {
-        
+        ret = table[base] + table[base + 2];
+    }
+    else if(left_num >= 5)
+    {
+        ret += table[base+1];
+        for(int i=0;i<left_num - 5;i++)
+        {
+            ret += table[base];
+        }
+    }
+    else if(left_num == 4)
+    {
+        ret = table[base] + table[base + 1];
+    }
+    else
+    {
+        for (size_t i = 0; i < left_num; i++)
+        {
+            ret += table[base];
+        }
     }
     
-    if(num >= 1000)
-    {
-        ret += "M";
-        ret += intToRoman(num % 1000);
-    }
-    else if(num >= 100)
-    {
-        
-        ret += intToRoman(num % 100);
-    }
-    
+    int _n = num % min_num;
+    return ret += intToRoman(_n);
 }
 
 int main(int argc, char* argv[])
 {
+    string ret = intToRoman(1994);
+    cout << "ret: "<<ret<<endl;
 
+    return 0;
 }
---
+++ Algorithms/LeetCode/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm> 
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int ret = pow(10, 2);
+    cout << ret << endl;
+}
